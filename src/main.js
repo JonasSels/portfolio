a1d04b789e1421f4740cc7e146f7e906dd8b072c
JonasSels <jonas.sels@student.pxl.be>
2025-05-22 00:35:55 +0200

    fix2

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+
 const app = createApp(App)
 
 app.use(router)
